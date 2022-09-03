e916af03782ff0f01ba188823bdcc7b3541150cd
Whitney Muchira <whitneymuchira@gmail.com>
2022-09-03 23:14:23 +0300

    commit tree fcdd22f7d8d6f951e2adb092130b05e53ae79b72

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+ def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator  array 
+    batches = array.map{|name| "Hello, my name is #{name}."}
+
+    batches
+
+end
+def assign_rooms array
+    list = []
+
+    list = []
+
+    array.length.times do |index|
+       list << "Hello, #{array[index]}! You'll be assigned to room #{index + 1}!"
+    end
+
+    list
+end
+def printer array
+    batch = batch_badge_creator(array)
+    room = assign_rooms(array)
+ 
+    batch.length.times do |index|
+     puts batch[index]
+    end
+ 
+    room.length.times do |index|
+     puts room[index]
+    end
+ end
